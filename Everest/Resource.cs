962117c8a7762f393d0b7dbf94a99e503e471308
Josh Chisholm <np-jchisholm@euromoneyplc.com>
2012-12-04 14:43:49 +0000

    pull up Url member

--- Everest/Resource.cs
+++ Everest/Resource.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using Everest.Content;
@@ -7,13 +8,15 @@ namespace Everest
 {
     public interface Resource
     {
+        Uri Url { get; }
+
         Response Get(string uri, IDictionary<string, string> headers, params PipelineOption[] options);
+        Response Get(string uri, params PipelineOption[] pipelineOptions);
         Response Post(string url, string body, params PipelineOption[] pipelineOptions);
         Response Put(string uri, string body, params PipelineOption[] pipelineOptions);
         Response Put(string uri, BodyContent body, params PipelineOption[] pipelineOptions);
         Response Head(string uri, params PipelineOption[] pipelineOptions);
         Response Delete(string uri, params PipelineOption[] pipelineOptions);
         Response Send(HttpMethod method, string uri, BodyContent body, params PipelineOption[] overridingPipelineOptions);
-        Response Get(string uri, params PipelineOption[] pipelineOptions);
     }
 }
