c6396dc79833c289ff4fb922b9f90c2ec2c7fe26
Josh Chisholm <np-jchisholm@euromoneyplc.com>
2013-01-04 13:18:18 +0000

    Expose status code in exception for unexpected response status

--- Everest/Status/UnexpectedStatusException.cs
+++ Everest/Status/UnexpectedStatusException.cs
@@ -4,8 +4,16 @@ namespace Everest.Status
 {
     public class UnexpectedStatusException : Exception
     {
+        private readonly int _statusCode;
+
         public UnexpectedStatusException(int statusCode, string message) : base(message)
         {
+            _statusCode = statusCode;
+        }
+
+        public int StatusCode
+        {
+            get { return _statusCode; }
         }
     }
 }
