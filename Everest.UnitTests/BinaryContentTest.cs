bdfbd99de0bc58f9cace698961c2178e5077bc33
martin bailey <martin.bailey@gmail.com>
2014-09-23 14:55:30 +0100

    added BodyAsStream

---
+++ Everest.UnitTests/BinaryContentTest.cs
@@ -0,0 +1,21 @@
+﻿using Everest.Headers;
+using NUnit.Framework;
+
+namespace Everest.UnitTests
+{
+    [TestFixture]
+    public class BinaryContentTest
+    {
+        [Test]
+        public void StreamsBinaryContent()
+        {
+            var response = new RestClient().Get("http://httpbin.org/stream-bytes/1024");
+
+            using ( var stream = response.BodyAsStream)
+            {
+                Assert.AreEqual(stream.Length,1024);
+            }
+
+        }
+    }
+}
